455970eb8e2d414f9919772ded4f8a889a0833e8
Saifk05 <kalkerisaif@gmail.com>
2026-06-10 14:38:03 +0530

    chore: create auth module structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,4 +10,12 @@ export const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full',
   },
+  {
+    path: 'login',
+    loadComponent: () => import('./features/auth/login/login.page').then( m => m.LoginPage)
+  },
+  {
+    path: 'register',
+    loadComponent: () => import('./features/auth/register/register.page').then( m => m.RegisterPage)
+  },
 ];
